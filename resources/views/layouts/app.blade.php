9833201ee9f558f4ccf0cf87bb173a38008b7c40
otdev1 <66022538+otdev1@users.noreply.github.com>
2020-07-29 20:39:19 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -124,7 +124,7 @@
                         <div class="row pt-5 mt-5 text-center">
                             <div class="col-md-12">
                                 <p>
-                                Copyright &copy; <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All Rights Reserved | This demo website is powered by Laravel | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+                                Copyright &copy; <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All Rights Reserved | This demo website is powered by Laravel | This template is an adaptation of the Job Start template by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
                                 </p>
                             </div>
                         </div>
